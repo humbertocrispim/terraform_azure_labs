5cc959bee96404993699ae9440e4c2de2f573242
humbertocrispim <humberto.cunha.crispim@gmail.com>
2022-09-05 13:38:16 -0300

    ok

---
+++ FOR-EXPRESSION-AWS-VPC/main.tf
@@ -0,0 +1,28 @@
+terraform {
+
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.73.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "curso-terraform-humbertocc"
+    key    = "aws-vpc-for-expression/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+
+  default_tags {
+    tags = {
+      owner      = "HumbertoCc"
+      managed-by = "terraform"
+    }
+  }
+}
---
+++ FOR-EXPRESSION-AWS-VPC/outputs.tf
@@ -0,0 +1,4 @@
+output "subnet_id" {
+  value = [for subnet in aws_subnet.subnet : subnet.id]
+
+}
